5d3b3aabe5fa1bd4910c8b90298fd3135eaa7265
Kishore0505 <kishorer0505@gmail.com>
2023-10-31 11:48:44 +0530

    first commit

---
+++ src/Index1.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+function Index1(){
+    const[counts1,setCounts1]=useState(0);
+    function myfunc()
+    {
+        setCounts1(counts1+1)
+    }
+    function myfunc1()
+    {
+        setCounts1(counts1-1);
+    }
+ return(
+    <>
+    <button onClick={myfunc}>Increase</button>
+    <span>{counts1}</span>
+    <button onClick={myfunc1}>Decrease</button>
+    </>
+ )
+}
+
+export default Index1 
---
+++ src/Kishore.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Kishore() {
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default Kishore
